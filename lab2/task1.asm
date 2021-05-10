053b8674dec537fca9250bacd6df7654b1363da8
Michał Szopiński <mszopinski@mssnt.pl>
2021-05-10 15:47:05 +0200

    Replace digits with arbitrary patterns

--- lab2/task1.asm
+++ lab2/task1.asm
@@ -8,37 +8,22 @@ DEC_CS      EQU     P3.2                ; chip select pin for decoder
 DEC_BUS     EQU     P3                  ; port of address bus (2 least significant bits used)
 SEG_BUS     EQU     P1                  ; segment bus
 
-            ORG     0F00h
-DIGITS:     DB      11000000b           ; digit pattern lookup table
-            DB      11111001b
-            DB      10100100b
-            DB      10110000b
-            DB      10011001b
-            DB      10010010b
-            DB      10000010b
-            DB      11111000b
-            DB      10000000b
-            DB      10010000b
-
-            ORG     0h
             clr     DEC_CS              ; clear output pin
             
-            mov     R0, #3              ; example call to digit displaying subroutine
-            mov     R1, #2              ; argument 1: position; argument 2: digit
-            call    showDigit
+            mov     R0, #3              ; example call to pattern displaying subroutine
+            mov     R1, #11110110b      ; argument 1: position; argument 2: pattern (pgfedcba)
+            call    showPatt
             
             jmp     $
 
-showDigit:  clr     DEC_CS              ; disable decoder
-
-            mov     A, R1               ; fetch digit pattern from memory
-            mov     DPL, #LOW(DIGITS)
-            mov     DPH, #HIGH(DIGITS)
-            movc    A, @A+DPTR
+showPatt:   clr     DEC_CS              ; disable decoder
+            
+            mov     A, R1               ; invert bits of pattern
+            xrl     A, #0FFh
             mov     SEG_BUS, A          ; move pattern to segment bus
             
-            mov     A, DEC_BUS
-            anl     A, #0FCh            ; clear bits of address bus
+            mov     A, DEC_BUS          ; clear 2 least significant bits of address bus
+            anl     A, #0FCh
             orl     A, R0               ; set address
             mov     DEC_BUS, A
             
